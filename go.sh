a6054e009ef16f1062994b39097873837623054d
Jim <sjyangv0@gmail.com>
2020-11-12 19:11:13 +0800

    update go.sh usage
    ./go.sh adb_device /dev/video
    
    Signed-off-by: Jim <sjyangv0@gmail.com>

--- go.sh
+++ go.sh
@@ -1,10 +1,11 @@
+adb -s $1 shell mkdir -p /data/$1
 count=0;
 while (($count < 1000000));
-do adb shell /data/capture  /dev/video3;
+do adb -s $1 shell /data/capture $2;
 ((count=$count+1));
 sleep 1
-adb shell mv *.yuv /data/yyy
-adb pull /data/yyy
-adb shell rm /data/yyy/* -f
+adb -s $1 shell mv "*.yuv" /data/$1
+adb -s $1 pull /data/$1
+adb -s $1 shell rm /data/$1/* -f
 echo $count
 done;
