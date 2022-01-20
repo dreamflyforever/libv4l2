dcd57c370a2cc5f04ae909b68d251ef3b4444403
jim <1150660744@qq.com>
2022-01-20 11:52:39 +0800

    improve set_exposure for camera
    Signed-off-by: jim <1150660744@qq.com>

--- example_capture.c
+++ example_capture.c
@@ -108,7 +108,7 @@ int main(int argc, char* argv[])
 
 	memset(&cap, 0, sizeof(cap));
 
-	if (argc != 2) {
+	if (argc != 3) {
 		/*check pid-vid search the device*/
 		#if 1
         	get_usbdevname("0b09","1bcf", video, dev_name);
@@ -117,11 +117,14 @@ int main(int argc, char* argv[])
         	//get_usbdevname("636b","0c45", video, dev_name);
 		snprintf(DEFAULT_DEV, 13, "/dev/%s", dev_name);
 		#endif
-		printf("Usage:%s </dev/videox> exposure, default: %s\n", argv[0], DEFAULT_DEV);
+		num = 80;
+		printf("Usage:%s </dev/videox> exposure, default: %s, exposure: %d\n",
+			argv[0], DEFAULT_DEV, num);
 		chdir("/data/");
 		//return -1;
 	} else {
 		strncpy(DEFAULT_DEV, argv[1], 13);
+		num = atoi(argv[2]);
 	}
 	printf("%s\n", DEFAULT_DEV);
 	t = time((time_t*)NULL);
@@ -156,7 +159,6 @@ int main(int argc, char* argv[])
 	if(cap.capabilities & V4L2_CAP_READWRITE)
 		printf("dev support read write\n");
 
-	num = atoi(argv[2]);
 	set_exposure(fd, num);
 	ret = v4l2_enuminput(fd, 0, name);
 	if(ret < 0)
